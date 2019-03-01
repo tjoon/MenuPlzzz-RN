5c70ad4759c154c7033cea0d2f33ff3897431c96
박세웅 <cloud@bagse-ung-ui-MacBookPro.local>
2019-03-01 22:23:30 +0900

    [Feat] HomeScreen 만들기
    eact-native-super-grid 설치
    패스트푸드 브랜드 별 버튼 만들기
    App.js 만들기

--- src/Screens/MenuScreen/index.js
+++ src/Screens/MenuScreen/index.js
@@ -1,1 +1,14 @@
-//index.js
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class Menu extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Menu </Text>
+      </View>
+    );
+  }
+}
+
+export default Menu;
