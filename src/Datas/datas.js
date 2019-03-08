fa2514889b444094970e81a989358d84b2a8793f
tjoon <bbjoejoe21@gmail.com>
2019-03-09 02:46:49 +0900

    [Feat] MenuScreen Map 사용
    Datas 폴더에 datas.js 더미 데이터 파일 만들기
    MenuItem.js 파일 만들어서 반복으로 사용할 ListItem 틀 만들기
    직접 입력한 카테고리 데이터를 map을 이용하여 반복문으로 해결하기
    MenuItem.js 파일 만들어서 ListItem 틀 만들기
    MenuScreen에서 매장별로 id값 구분하여 선택한 매장의 메뉴가 보이는 처리

---
+++ src/Datas/datas.js
@@ -0,0 +1,353 @@
+export const macdonald = [
+  {
+    category: "추천 메뉴",
+    menu: [
+      {
+        name: "골든 에그 치즈버거",
+        price: "9,000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7523.png?"
+      },
+      {
+        name: "그릴드 머쉬룸 버거",
+        price: "9,000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7524.png?"
+      },
+      {
+        name: "에그 불고기 버거",
+        price: "6,500",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5318.png?"
+      }
+    ]
+  },
+  {
+    category: "버거 & 세트",
+    menu: [
+      {
+        name: "골든 에그 치즈버거",
+        price: "7,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7513.png?"
+      },
+      {
+        name: "그릴드 머쉬룸 버거",
+        price: "7,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7514.png?"
+      },
+      {
+        name: "에그 불고기 버거",
+        price: "3,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5317.png?"
+      }
+    ]
+  },
+  {
+    category: "스낵 & 사이드",
+    menu: [
+      {
+        name: "상하이 치킨 스낵랩",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1260.png?"
+      },
+      {
+        name: "골든 모짜렐라 치즈스틱 2조각",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/6991.png?"
+      },
+      {
+        name: "골든 모짜렐라 치즈스틱 4조각",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/6992.png?"
+      }
+    ]
+  },
+  {
+    category: "음료",
+    menu: [
+      {
+        name: "딸기 칠러",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1852.png?"
+      },
+      {
+        name: "자두 칠러",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1630.png?"
+      },
+      {
+        name: "코카-콜라",
+        price: "1,800",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1506.png?"
+      }
+    ]
+  },
+  {
+    category: "디저트",
+    menu: [
+      {
+        name: "애플 파이",
+        price: "1,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5264.png?"
+      },
+      {
+        name: "오레오 맥플러리",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1732.png?"
+      },
+      {
+        name: "초코 선데이 아이스크림",
+        price: "2,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1712.png?"
+      }
+    ]
+  }
+];
+
+export const momstouch = [
+  {
+    category: "맘스터치",
+    menu: [
+      {
+        name: "골든 에그 치즈버거",
+        price: "9,000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7523.png?"
+      },
+      {
+        name: "그릴드 머쉬룸 버거",
+        price: "9,000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7524.png?"
+      },
+      {
+        name: "에그 불고기 버거",
+        price: "6,500",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5318.png?"
+      }
+    ]
+  },
+  {
+    category: "버거 & 세트",
+    menu: [
+      {
+        name: "골든 에그 치즈버거",
+        price: "7,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7513.png?"
+      },
+      {
+        name: "그릴드 머쉬룸 버거",
+        price: "7,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7514.png?"
+      },
+      {
+        name: "에그 불고기 버거",
+        price: "3,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5317.png?"
+      }
+    ]
+  },
+  {
+    category: "스낵 & 사이드",
+    menu: [
+      {
+        name: "상하이 치킨 스낵랩",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1260.png?"
+      },
+      {
+        name: "골든 모짜렐라 치즈스틱 2조각",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/6991.png?"
+      },
+      {
+        name: "골든 모짜렐라 치즈스틱 4조각",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/6992.png?"
+      }
+    ]
+  },
+  {
+    category: "음료",
+    menu: [
+      {
+        name: "딸기 칠러",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1852.png?"
+      },
+      {
+        name: "자두 칠러",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1630.png?"
+      },
+      {
+        name: "코카-콜라",
+        price: "1,800",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1506.png?"
+      }
+    ]
+  },
+  {
+    category: "디저트",
+    menu: [
+      {
+        name: "애플 파이",
+        price: "1,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5264.png?"
+      },
+      {
+        name: "오레오 맥플러리",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1732.png?"
+      },
+      {
+        name: "초코 선데이 아이스크림",
+        price: "2,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1712.png?"
+      }
+    ]
+  }
+];
+
+export const kfc = [
+  {
+    category: "KFC",
+    menu: [
+      {
+        name: "골든 에그 치즈버거",
+        price: "9,000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7523.png?"
+      },
+      {
+        name: "그릴드 머쉬룸 버거",
+        price: "9,000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7524.png?"
+      },
+      {
+        name: "에그 불고기 버거",
+        price: "6,500",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5318.png?"
+      }
+    ]
+  },
+  {
+    category: "버거 & 세트",
+    menu: [
+      {
+        name: "골든 에그 치즈버거",
+        price: "7,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7513.png?"
+      },
+      {
+        name: "그릴드 머쉬룸 버거",
+        price: "7,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/7514.png?"
+      },
+      {
+        name: "에그 불고기 버거",
+        price: "3,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5317.png?"
+      }
+    ]
+  },
+  {
+    category: "스낵 & 사이드",
+    menu: [
+      {
+        name: "상하이 치킨 스낵랩",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1260.png?"
+      },
+      {
+        name: "골든 모짜렐라 치즈스틱 2조각",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/6991.png?"
+      },
+      {
+        name: "골든 모짜렐라 치즈스틱 4조각",
+        price: "9000",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/6992.png?"
+      }
+    ]
+  },
+  {
+    category: "음료",
+    menu: [
+      {
+        name: "딸기 칠러",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1852.png?"
+      },
+      {
+        name: "자두 칠러",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1630.png?"
+      },
+      {
+        name: "코카-콜라",
+        price: "1,800",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1506.png?"
+      }
+    ]
+  },
+  {
+    category: "디저트",
+    menu: [
+      {
+        name: "애플 파이",
+        price: "1,600",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/5264.png?"
+      },
+      {
+        name: "오레오 맥플러리",
+        price: "3,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1732.png?"
+      },
+      {
+        name: "초코 선데이 아이스크림",
+        price: "2,100",
+        image:
+          "https://www.mcdelivery.co.kr/kr/static/1550824543513/assets/82/products/1712.png?"
+      }
+    ]
+  }
+];
--- src/Screens/MenuScreen/index.js
+++ src/Screens/MenuScreen/index.js
@@ -1,34 +1,50 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
-import { Container, Tab, Tabs, ScrollableTab } from 'native-base';
+import { Container, Tab, Tabs, ScrollableTab } from "native-base";
 
-import Tab1 from "../../Components/Tab1"
-import Tab2 from "../../Components/Tab2"
-import Tab3 from "../../Components/Tab3"
-import Tab4 from "../../Components/Tab4"
-import Tab5 from "../../Components/Tab5"
+import Tab1 from "../../Components/Tab1";
+import Tab2 from "../../Components/Tab2";
+import Tab3 from "../../Components/Tab3";
+import Tab4 from "../../Components/Tab4";
+import Tab5 from "../../Components/Tab5";
+
+import { kfc, macdonald, momstouch } from "../../Datas/datas";
+import ChildTab from "../../Components/ChildTab";
 
 export class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      datas: []
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.store_id == 0) {
+      this.setState({
+        datas: macdonald
+      });
+    } else if (this.props.store_id == 1) {
+      this.setState({
+        datas: momstouch
+      });
+    } else if (this.props.store_id == 2) {
+      this.setState({
+        datas: kfc
+      });
+    }
+  }
+
   render() {
+    const menuList = this.state.datas.map((ele, index) => (
+      <Tab key={index} heading={ele.category}>
+        <ChildTab menu={ele.menu} />
+      </Tab>
+    ));
     return (
       <Container>
-        <Tabs renderTabBar={()=> <ScrollableTab />}>
-          <Tab heading="추천 메뉴">
-            <Tab1 />
-          </Tab>
-          <Tab heading="버거 & 세트">
-            <Tab2 />
-          </Tab>
-          <Tab heading="스낵 & 사이드">
-            <Tab3 />
-          </Tab>
-          <Tab heading="음료">
-            <Tab4 />
-          </Tab>
-          <Tab heading="디저트">
-            <Tab5 />
-          </Tab>
-        </Tabs>
+        <Tabs renderTabBar={() => <ScrollableTab />}>{menuList}</Tabs>
       </Container>
     );
   }
