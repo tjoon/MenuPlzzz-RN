b6950f081d3b37c3e22af439ad78ad408a2a9a66
John Doe <kth5604@gmail.com>
2019-05-05 17:01:56 +0900

    Feat: ImageLoad로 교체

--- src/Screens/MenuScreen/Presenter.js
+++ src/Screens/MenuScreen/Presenter.js
@@ -1,25 +1,22 @@
 import {
   Container,
   Content,
-  Thumbnail,
   Text,
   List,
   ListItem,
   Left,
   Right,
-  Body
+  Body,
 } from "native-base";
 import PropTypes from "prop-types";
 import React from "react";
 import { StyleSheet } from "react-native";
+import ImageLoad from "react-native-image-placeholder";
 
 const ChildTab = props => {
-  console.log("kwontaehyoung");
   const menuListItem = props.menu.map((ele, index) => (
     <MenuItem key={index} image={ele.image} name={ele.name} price={ele.price} />
   ));
-
-  console.log(menuListItem);
 
   return (
     <Container>
@@ -34,10 +31,11 @@ const MenuItem = props => {
   return (
     <ListItem style={styles.container}>
       <Left style={styles.left}>
-        <Thumbnail
-          square
+        <ImageLoad
+          style={{ width: 45, height: 45 }}
+          loadingStyle={{ size: "large", color: "black" }}
           source={{
-            uri: props.image
+            uri: props.image,
           }}
         />
       </Left>
@@ -54,31 +52,32 @@ const MenuItem = props => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
 
   left: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   body: {
     flex: 2,
-    flexDirection: "column"
+    flexDirection: "column",
   },
   right: {
-    flex: 1
-  }
+    flex: 1,
+    marginRight: 15,
+  },
 });
 
 ChildTab.propTypes = {
-  menu: PropTypes.array
+  menu: PropTypes.array,
 };
 
 MenuItem.propTypes = {
   image: PropTypes.string,
   name: PropTypes.string,
-  price: PropTypes.string
+  price: PropTypes.string,
 };
 
 export default ChildTab;
